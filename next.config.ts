52d865a23089bdd8614a7a878e67b5fa59c6fed5
cleberbazzano <cleberbazzano@hotmail.com>
2026-02-22 21:19:32 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
